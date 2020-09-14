92a7961aa1330ff0b2e41d52e089be08fee2ee18
Benjamin Ramauge <ramauge.benjamin@gmail.com>
2020-09-14 20:41:07 +0200

    Add playlist

---
+++ src/views/Playlist.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <player
+      :v-for="(music, index) in musics"
+      v-bind:source="music.source"
+      v-bind:title="music.title"
+      :key="index"
+    ></player>
+    <!-- <player :value="musics"></player> -->
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import Player from '@/components/Player.vue';
+import { Sound } from '../models/sound';
+
+export default defineComponent({
+  name: 'Playlist',
+  components: {
+    Player,
+  },
+  data() {
+    return {
+      musics: [{
+        source: '/AuRa-01-02-Ghost.mp3',
+        title: 'Ghost',
+        start: 0,
+        volume: 1,
+        loopable: true,
+      }] as Array<Sound>,
+    };
+  },
+});
+</script>
