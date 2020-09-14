0ec0636942b9cc8a119ddc73044fb13496878936
Benjamin Ramauge <ramauge.benjamin@gmail.com>
2020-09-14 20:40:47 +0200

    Add Player

---
+++ src/components/Player.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <audio controls preload="metadata">
+      <source :src=source />
+    </audio>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+// import { defineComponent, PropType } from 'vue';
+// import { Sound } from '../models/sound';
+
+export default defineComponent({
+  name: 'Player',
+  props: {
+    // value: {
+    //   type: Object as PropType<Sound>,
+    //   required: true,
+    // },
+    title: String,
+    source: String,
+  },
+  data() {
+    return {
+      // sound: this.value,
+    };
+  },
+});
+</script>
